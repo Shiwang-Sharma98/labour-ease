e8c6210ef81f9ac9142f1b933881a45494085385
Shiwang-Sharma98 <147147034+Shiwang-Sharma98@users.noreply.github.com>
2025-05-11 09:32:46 +0530

    tailwind added

---
+++ src/app/layout.js
@@ -0,0 +1,30 @@
+import './globals.css';
+import { Geist, Geist_Mono } from 'next/font/google';
+import { Providers } from './providers';
+
+const geistSans = Geist({
+  variable: '--font-geist-sans',
+  subsets: ['latin'],
+});
+
+const geistMono = Geist_Mono({
+  variable: '--font-geist-mono',
+  subsets: ['latin'],
+});
+
+export const metadata = {
+  title: 'Labour Ease',
+  description: 'Platform for managing labour services',
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" suppressHydrationWarning>
+      <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        <Providers>
+          {children}
+        </Providers>
+      </body>
+    </html>
+  );
+}
---
+++ src/app/page.js
@@ -0,0 +1,11 @@
+import { sql } from "@vercel/postgres";
+import Home from "./components/Home";
+
+export default async function Main() {
+
+  return (
+    <main>
+      <Home />
+    </main>
+  );
+}
